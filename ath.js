751eb70022fec1a8d1c41f07c4965b56ce5ff5e1
seeedy <chrstn.sdntp@gmail.com>
2021-05-04 15:04:57 +0200

    initial commit

---
+++ ath.js
@@ -0,0 +1,45 @@
+const axios = require('axios');
+
+console.log('Bot started...');
+
+var date = new Date();
+var today = date.toISOString().slice(0, 10);
+
+var ydayDate = new Date(date.setDate(date.getDate() - 1));
+var yesterday = ydayDate.toISOString().slice(0, 10);
+
+var todayATHs = [];
+var ydayATHs = [];
+
+axios
+  .get('https://api.coingecko.com/api/v3/coins/markets', {
+    params: {
+      vs_currency: 'usd',
+      order: 'market_cap_desc',
+    },
+  })
+  .then((resp) => {
+    const { data } = resp;
+
+    data.map((elem) => {
+      var ath = elem.ath_date.slice(0, 10);
+      if (ath === today) todayATHs.push(elem.id);
+      if (ath === yesterday) ydayATHs.push(elem.id);
+      return todayATHs;
+    });
+
+    console.log(`
+Today ${today} ATHs:
+------------------------------
+`);
+    todayATHs.map((ath) => console.log(ath));
+    console.log(`
+
+Yesterday ${yesterday} ATHs:
+------------------------------
+`);
+    ydayATHs.map((ath) => console.log(ath));
+    console.log(`
+------------------------------
+------------------------------`);
+  });
